a1c4fad0e5e2cd277734e24dcd51bdf6b3f72b6e
ben <benzoic09@gmail.com>
2024-01-19 06:20:12 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 #!/usr/bin/pup
-# Installing flask to get the api info
-
-package { 'flask':
+# Install an especific version of flask (2.1.0)
+package {'flask':
   ensure   => '2.1.0',
-  provider => 'pip'
+  provider => 'pip3'
 }
