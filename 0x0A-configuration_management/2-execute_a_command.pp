09cb84bc14da9a1a375950f0f3c9b6574f14fa97
ben <benzoic09@gmail.com>
2024-01-19 09:15:34 -0800

    task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,14 +1,8 @@
 #!/usr/bin/puppet
 
 # Define a Puppet exec resource to kill the process
-exec { 'killmenow_process':
+exec { 'pkill -f killmenow':
   command     => '/usr/bin/pkill -f killmenow',
   refreshonly => true,
   logoutput   => true,
 }
-
-package {'Werkzeug':
-  ensure   => '2.1.1',
-  provider => 'pip3'
-}
-
