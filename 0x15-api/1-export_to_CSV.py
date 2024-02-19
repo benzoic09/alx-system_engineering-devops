33ac8d54de577a81d3522a9f41f4b23f7bb15638
ben <benzoic09@gmail.com>
2024-02-19 11:38:08 -0800

    task one

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-"""Python script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress and
-exports data in CSV format."""
+"""
+Python script that, using a REST API, for a given employee ID,
+returns information about his/her TODO list
+progress and exports data in CSV format.
+"""
 import csv
 import requests
 import sys
@@ -10,21 +12,35 @@ if __name__ == "__main__":
     user_id = sys.argv[1]
 
     # Fetch user information
-    url = "https://jsonplaceholder.typicode.com/"
-    user = requests.get(url + "users/{}".format(user_id)).json()
-    username = user.get("username")
+    user_response = requests.get(
+            "https://jsonplaceholder.typicode.com/users/{}".format(user_id))
+    if user_response.status_code != 200:
+        print("User not found.")
+        sys.exit(1)
+
+    user_data = user_response.json()
+    username = user_data.get("username")
 
     # Fetch user's tasks
-    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+    todos_response = requests.get(
+            "https://jsonplaceholder.typicode.com/todos", params={"userId": user_id})
+    if todos_response.status_code != 200:
+        print("Tasks not found for the user.")
+        sys.exit(1)
+
+    todos = todos_response.json()
 
     # Prepare CSV data
     csv_data = [["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"]]
     for task in todos:
         task_completed_status = "Yes" if task.get('completed') else "No"
-        csv_data.append(
-                [user_id, username, task_completed_status, task.get('title')])
+        csv_data.append([user_id, username, task_completed_status, task.get('title')])
 
     # Write CSV data to file
-    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
-        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
-        writer.writerows(csv_data)
+    csv_filename = "{}.csv".format(user_id)
+    with open(csv_filename, "w", newline="") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerows(csv_data)
+
+    print("CSV file '{}' has been generated.".format(csv_filename))
+
