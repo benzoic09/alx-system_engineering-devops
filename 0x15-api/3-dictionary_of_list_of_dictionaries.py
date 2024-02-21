3270c2348601fc696be5a3e667c1ac2d968f6f12
ben <benzoic09@gmail.com>
2024-02-21 12:51:46 -0800

    task 4

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -11,11 +11,24 @@ if __name__ == "__main__":
     todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
     users = requests.get('https://jsonplaceholder.typicode.com/users').json()
 
+    # Create a dictionary to store data for all employees
+    all_data = {}
+
+    for user in users:
+        user_id = str(user['id'])
+        username = user['username']
+
+        user_tasks = []
+        for task in todos:
+            if task['userId'] == user['id']:
+                user_tasks.append({
+                    "username": username,
+                    "task": task["title"],
+                    "completed": task["completed"]
+                })
+
+        all_data[user_id] = user_tasks
+
     # Write data to JSON file
     with open("todo_all_employees.json", 'w') as jsonfile:
-        json.dump({
-            u.get("id"): [{"task": t.get("title"),
-                          "completed": t.get("completed"), "username": u.get("username")
-                           } for t in requests.get(todos,
-                                                   params={"userId": u.get("id")}).json()]
-            for u in users}, jsonfile)
+        json.dump(all_data, jsonfile, indent=4)
