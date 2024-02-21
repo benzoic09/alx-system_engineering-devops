c6a04475a87f783964c21b2e2294f229e889e862
ben <benzoic09@gmail.com>
2024-02-21 10:15:08 -0800

    export to json

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,41 +1,28 @@
 #!/usr/bin/python3
 """
-Python script that, using a REST API, for a given employee ID,
-returns information about his/her TODO list progress and exports data in JSON format.
+Python script that, using a REST API, exports data in JSON format for all employees' tasks.
 """
+
 import json
 import requests
 import sys
 
 if __name__ == "__main__":
-    user_id = sys.argv[1]
+    todos = requests.get('https://jsonplaceholder.typicode.com/todos')
+    users = requests.get('https://jsonplaceholder.typicode.com/users')
 
-    # Fetch user information
-    user_response = requests.get("https://jsonplaceholder.typicode.com/users/{}".format(user_id))
-    if user_response.status_code != 200:
-        print("User not found.")
-        sys.exit(1)
+    # Organize tasks by user ID
+    tasks_by_user = {}
+    for task in todos.json():
+        userId = sys.argv[1]
+        if userId not in tasks_by_user:
+            tasks_by_user[userId] = []
+        tasks_by_user[userId].append({
+            'username': [user['username'] for user in users.json() if user['id'] == userId],
+            'task': task.get('title'),
+            'completed': task.get('completed')
+        })
 
-    user_data = user_response.json()
-    username = user_data.get("username")
-
-    # Fetch user's tasks
-    todos_response = requests.get("https://jsonplaceholder.typicode.com/todos", params={"userId": user_id})
-    if todos_response.status_code != 200:
-        print("Tasks not found for the user.")
-        sys.exit(1)
-
-    todos = todos_response.json()
-
-    # Prepare JSON data
-    json_data = {"USER_ID": []}
-    for task in todos:
-        task_completed_status = "Yes" if task.get('completed') else "No"
-        json_data["USER_ID"].append({"task": task.get('title'), "completed": task_completed_status, "username": username})
-
-    # Write JSON data to file
-    json_filename = "{}.json".format(user_id)
-    with open(json_filename, "w") as jsonfile:
-        json.dump(json_data, jsonfile, indent=4)
-
-    print("JSON file '{}' has been generated.".format(json_filename))
+    # Write data to JSON file
+    with open("{}.json".format(userId), 'w') as jsonfile:
+        json.dump(tasks_by_user, jsonfile, indent=4)
