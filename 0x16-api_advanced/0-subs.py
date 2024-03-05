5b96e8dc37b78dd6f6ffc4543a82549a70566ace
ben <benzoic09@gmail.com>
2024-03-05 04:33:32 -0800

    task zero

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,9 @@ import requests
 
 
 def number_of_subscribers(subreddit):
+    """
+     queries the Reddit API and returns the number of subscribers
+     """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     headers = {"User-Agent": "Custom User Agent"}
