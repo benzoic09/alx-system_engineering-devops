5e17900c987a7721d761ab2cfe18697556000400
ben <benzoic09@gmail.com>
2024-03-05 09:22:13 -0800

    task 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -12,12 +12,12 @@ def top_ten(subreddit):
 
     headers = {"User-Agent": "Custom User Agent"}
 
-    response = request.get(url, header=header, allow_redirects=False)
+    response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
         posts = data['data']['children']
         for post in posts:
             print(post['data']['title'])
-        else:
-            print(None)
+    else:
+        print(None)
