da3de2c1aad57e7a69e062096a32ab133a401b29
ben <benzoic09@gmail.com>
2024-03-05 06:19:35 -0800

    task one

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""task one"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Queries the Reddit API and returns the top 10 hot posts
+    """
+    url = "https://www.reddit.com/r/{}/hot.json?limit=10".format(subreddit)
+
+    headers = {"User-Agent": "Custom User Agent"}
+
+    response = request.get(url, header=header, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+        else:
+            print(None)
