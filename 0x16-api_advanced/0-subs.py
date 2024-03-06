c6ad9f35ced353ed0a8f46c684986958ad216e82
ben <benzoic09@gmail.com>
2024-03-06 06:07:31 -0800

    task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,21 +1,17 @@
 #!/usr/bin/python3
 """Module for task 0"""
 
+import requests
 
 def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
-    import requests
-
-    # Check if subreddit is a valid string
-    if not isinstance(subreddit, str):
-        return 0
 
     # Construct the URL for subreddit information
     sub_info_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
     # Set custom User-Agent header
-    headers = {"User-Agent": "CustomClient/1.0"}
+    headers = {"User-Agent":"Ubuntu 20.04"}
 
     # Make the GET request to the subreddit information URL
     response = requests.get(
