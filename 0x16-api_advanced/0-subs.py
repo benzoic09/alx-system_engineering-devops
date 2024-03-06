ed0c086c8f69094786b4343714c7c7aab8693a5a
ben <benzoic09@gmail.com>
2024-03-06 05:13:47 -0800

    task zero

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,21 +6,23 @@ def number_of_subscribers(subreddit):
     """Queries the Reddit API and returns the number of subscribers
     to the subreddit"""
     import requests
-    if subreddit is None or not isinstance(subreddit, str):
+
+    # Check if subreddit is a valid string
+    if not isinstance(subreddit, str):
         return 0
 
+    # Construct the URL for subreddit information
     sub_info_url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
 
+    # Set custom User-Agent header
     headers = {"User-Agent": "CustomClient/1.0"}
 
+    # Make the GET request to the subreddit information URL
     response = requests.get(
         sub_info_url, headers=headers, allow_redirects=False)
 
-    if response.status_code != 200:
-        return (0)
-    response = response.json()
-    if 'data' in response:
-        return (response.get('data').get('subscribers'))
-
-    else:
-        return (0)
+    if response.status_code == 200:
+        data = response.json().get("data")
+        if data and "subscribers" in data:
+            return data["subscribers"]
+    return 0
