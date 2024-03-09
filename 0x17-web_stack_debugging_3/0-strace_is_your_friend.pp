cb6fd8e5bddf18582bc40e23b6823957619b3174
ben <benzoic09@gmail.com>
2024-03-09 00:04:39 -0800

    webstack debugging

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # puppet script that fixes the 500 error 
-exec { '/var/www/html/wp-setting.php':
-
-path => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+#
+# exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
 }
