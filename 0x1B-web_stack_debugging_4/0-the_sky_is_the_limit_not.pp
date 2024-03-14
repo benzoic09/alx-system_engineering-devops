ba4254602dee5078b03573e1c42effb9dacb687e
ben <benzoic09@gmail.com>
2024-03-14 07:09:08 -0700

    debugging 4

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,14 @@
 # Puppet manifest to fix server errors in Nginx server
 
 # Define Nginx configuration file
-file { '/etc/nginx/nginx.conf':
-  ensure  => file,
-  owner   => 'root',
-  group   => 'root',
-  mode    => '0644',
-  content => template('nginx/nginx.conf.erb'),
-  notify  => Service['nginx'],
-}
+#file { '/etc/nginx/nginx.conf':
+  #ensure  => file,
+  #owner   => 'root',
+  #group   => 'root',
+  #mode    => '0644',
+  #content => template('nginx/nginx.conf.erb'),
+  #notify  => Service['nginx'],
+#}
 
 # Define Exec resource to run the script
 exec { 'fix_nginx_errors':
