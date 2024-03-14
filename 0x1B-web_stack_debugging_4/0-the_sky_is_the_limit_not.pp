165b77ecf89008adef559ad1b44118d1626f3c49
ben <benzoic09@gmail.com>
2024-03-14 06:05:32 -0700

    debugging

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,4 +1,4 @@
-# Puppet manifest to fix multiple server errors in Nginx server
+# Puppet manifest to fix server errors in Nginx server
 
 # Define Nginx configuration file
 file { '/etc/nginx/nginx.conf':
@@ -11,17 +11,18 @@ file { '/etc/nginx/nginx.conf':
 }
 
 # Define Exec resource to run the script
-exec { 'fix_failed_requests':
-  command     => '/usr/local/bin/fix_failed_requests.sh',
-  refreshonly => true,  # Only run when the nginx.conf file changes
+exec { 'fix_nginx_errors':
+  command     => '/usr/local/bin/fix_nginx_errors.sh',
+  path        => ['/usr/local/bin', '/bin', '/usr/bin'],
+  refreshonly => true,
   subscribe   => File['/etc/nginx/nginx.conf'],
 }
 
 # Define the script file
-file { '/usr/local/bin/fix_failed_requests.sh':
+file { '/usr/local/bin/fix_nginx_errors.sh':
   ensure  => file,
   owner   => 'root',
   group   => 'root',
   mode    => '0755',
-  content => template('nginx/fix_failed_requests.sh.erb'),
+  content => template('nginx/fix_nginx_errors.sh.erb'),
 }
