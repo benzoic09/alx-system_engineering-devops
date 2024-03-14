1433f18f4c238b289bde77773ac519d8e6d525c3
ben <benzoic09@gmail.com>
2024-03-14 06:59:57 -0700

    debugging 4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,5 @@
+#user holberton to login and open files without error.
+
 # Increase hard file limit for Holberton user.
 exec { 'increase-hard-file-limit-for-holberton-user':
   command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf',
